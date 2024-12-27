b0397868d2776fba6d943029500aadcb896e599f
QyYy123 <76424186+QyYy123@users.noreply.github.com>
2024-12-27 14:31:41 +0800

    Update test_options.py

--- options/test_options.py
+++ options/test_options.py
@@ -9,7 +9,7 @@ class TestOptions(BaseOptions):
 
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)  # define shared options
-        parser.add_argument('--results_dir', type=str, default='data1/qyy/CycleH-CUT/Result/OASIS/new', help='saves results here.')
+        parser.add_argument('--results_dir', type=str, default='', help='saves results here.')
         parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
         # Dropout and Batchnorm has different behavioir during training and test.
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
